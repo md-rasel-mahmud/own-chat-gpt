fc7fe67a86a377b6deeff3c64383190e5ff9fd39
myself-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2023-07-09 23:30:29 +0600

    Optimize code and design. Start javascript

---
+++ js/app.js
@@ -0,0 +1,32 @@
+//NOTE: I am using this api key for demo purpose so I am showing this apiKey publicly. But whenever I use this API key in production I will use the API key by environment variable.
+const apiKey = "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";
+
+/**
+ * TODO:
+ * Write JavaScript code for user input:
+    Add an event listener to capture user input from the input box.
+    Store the user's message in a variable or data structure.
+    Trigger a function to handle the user's message.
+ * Implement API request handling:
+    Use the fetch() or XMLHttpRequest API to send a POST request to the ChatGPT API endpoint.
+    Include the necessary headers, including the API key, in the request.
+    Pass the user's message as the input for the API request.
+    Handle the API response and extract the generated model response.
+ * Update the conversation interface:
+    Append the user's message and generated model response to the chat area.
+    Format and style the conversation history to distinguish between user and model messages.
+    Scroll the chat area to the bottom to show the latest messages.
+ * Test the application:
+    Test the application by sending different messages and verifying the responses.
+    Check for any errors or unexpected behavior.
+    Debug and fix issues as needed.
+ * Refactor and optimize the code:
+    Review and optimize the JavaScript code for efficiency and readability.
+    Consider modularizing the code into separate functions or modules.
+    Remove any redundant or unused code.
+    Ensure that the code follows best practices and is maintainable.
+ * Document the code:
+    Add comments to explain important implementation details.
+    Provide usage instructions for running the application.
+    Include any specific instructions for obtaining and using the API key.
+ * */
